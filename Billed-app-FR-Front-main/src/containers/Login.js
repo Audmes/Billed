124ed8a50be71a85a1da1c8f2f1e4fa8a48515d2
Audmes <102157665+Audmes@users.noreply.github.com>
2024-04-02 16:07:50 +0200

    Commit : 02/04/2024 - Fix bug Issue 2 : Correction sur le fichier containers/Login.js - Test : Ok

--- Billed-app-FR-Front-main/src/containers/Login.js
+++ Billed-app-FR-Front-main/src/containers/Login.js
@@ -1,80 +1,81 @@
 
-import { ROUTES_PATH } from '../constants/routes.js'
-export let PREVIOUS_LOCATION = ''
+import { ROUTES_PATH } from '../constants/routes.js';
+export let PREVIOUS_LOCATION = '';
 
 // we use a class so as to test its methods in e2e tests
 export default class Login {
   constructor({ document, localStorage, onNavigate, PREVIOUS_LOCATION, store }) {
-    this.document = document
-    this.localStorage = localStorage
-    this.onNavigate = onNavigate
-    this.PREVIOUS_LOCATION = PREVIOUS_LOCATION
-    this.store = store
-    const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`)
-    formEmployee.addEventListener("submit", this.handleSubmitEmployee)
-    const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`)
-    formAdmin.addEventListener("submit", this.handleSubmitAdmin)
-  }
-  handleSubmitEmployee = e => {
-    e.preventDefault()
+    this.document = document;
+    this.localStorage = localStorage;
+    this.onNavigate = onNavigate;
+    this.PREVIOUS_LOCATION = PREVIOUS_LOCATION;
+    this.store = store;
+    const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`);
+    formEmployee.addEventListener("submit", this.handleSubmitEmployee);
+    const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`);
+    formAdmin.addEventListener("submit", this.handleSubmitAdmin);
+  };
+
+  handleSubmitEmployee = (e) => {
+    e.preventDefault();
     const user = {
       type: "Employee",
       email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
-      status: "connected"
-    }
-    this.localStorage.setItem("user", JSON.stringify(user))
+      status: "connected",
+    };
+    this.localStorage.setItem("user", JSON.stringify(user));
     this.login(user)
-      .catch(
-        (err) => this.createUser(user)
-      )
+      // .catch((err) => this.createUser(user))
       .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-        this.PREVIOUS_LOCATION = ROUTES_PATH['Bills']
-        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        this.document.body.style.backgroundColor="#fff"
-      })
+        this.onNavigate(ROUTES_PATH['Bills']);
+        this.PREVIOUS_LOCATION = ROUTES_PATH['Bills'];
+        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
+        this.document.body.style.backgroundColor="#fff";
+      });
+  };
 
-  }
-
-  handleSubmitAdmin = e => {
+  /* Fix bug Issue 2 */
+  handleSubmitAdmin = (e) => {
     e.preventDefault()
     const user = {
       type: "Admin",
-      /* Fix bug Issue 2 *//* employee est changé par admin */
+      /* employee est changé par admin */
       email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
-      status: "connected"
-    }
-    this.localStorage.setItem("user", JSON.stringify(user))
+      status: "connected",
+    };
+    this.localStorage.setItem("user", JSON.stringify(user));
     this.login(user)
-      .catch(
-        (err) => this.createUser(user)
-      )
+      // .catch((err) => this.createUser(user))
       .then(() => {
-        this.onNavigate(ROUTES_PATH['Dashboard'])
-        this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
-        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        document.body.style.backgroundColor="#fff"
-      })
-  }
+        this.onNavigate(ROUTES_PATH['Dashboard']);
+        this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard'];
+        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
+        document.body.style.backgroundColor="#fff";
+      });
+  };
 
   // not need to cover this function by tests
+  /* istanbul ignore next */
   login = (user) => {
     if (this.store) {
       return this.store
       .login(JSON.stringify({
         email: user.email,
         password: user.password,
-      })).then(({jwt}) => {
+      }))
+      .then(({jwt}) => {
         localStorage.setItem('jwt', jwt)
       })
+      .catch((error) => error);
     } else {
-      return null
+      return null;
     }
-  }
+  };
 
   // not need to cover this function by tests
+  /* istanbul ignore next */
   createUser = (user) => {
     if (this.store) {
       return this.store
@@ -89,8 +90,9 @@ export default class Login {
         console.log(`User with ${user.email} is created`)
         return this.login(user)
       })
+      .catch((error) => error);
     } else {
-      return null
+      return null;
     }
-  }
-}
+  };
+};
